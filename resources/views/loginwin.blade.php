02b9fc76ddb94b79139c7647af9d5c04104f8359
haseeAmarathunga <haseeamarathunga@gmail.com>
2018-11-13 12:31:35 -0800

    Update loginwin

--- resources/views/loginwin.blade.php
+++ resources/views/loginwin.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-    <img src="img/b1.jpg">
+    <img src="img/a.jpg">
 
-      <div class ="well"><div class="col-lg-6"></div><div class = "col-lg-6"><button type="button" class="btn btn-success btn-lg btn-block" on-click="admin()"><span class="glyphicon glyphicon-user"></span> Admin</button></div><div class = "col-lg-6"></div></div>
+      <div class ="well"><div class="col-lg-6"></div><div class = "col-lg-6"><button type="button" class="btn btn-primary btn-lg btn-block" on-click="admin()"><span class="glyphicon glyphicon-user"></span> Admin</button></div><div class = "col-lg-6"></div></div>
 
       <div class ="well"><div class="col-lg-6"></div><div class = "col-lg-6"><button type="button" class="btn btn-warning btn-lg btn-block"  on-click="farmshop()"><span class="glyphicon glyphicon-credit-card"></span> Staff</button></div><div class = "col-lg-6"></div></div>
       
