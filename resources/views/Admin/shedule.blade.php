8a0532df9a272e37b711056259965a1ad134492e
j.p.s sasadara <jpssasadara1995@gmail.com>
2018-11-22 21:13:20 +0530

    add form

--- resources/views/Admin/shedule.blade.php
+++ resources/views/Admin/shedule.blade.php
@@ -20,8 +20,31 @@
 
 <!-- buttons -->
 
-<div>
+<div class="container">
 <h1>shedules</h1>
+<div class ="col-md-4 well">
+  Add Item
+  <br>
+  <form action="/action_page.php">
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="form-group form-check">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox"> Remember me
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+<div class ="col-md-8 well">
+  View Shedule
+</div>
 </div>
 
 
