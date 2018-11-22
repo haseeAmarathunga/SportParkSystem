c1d9ea5588d91f206dc676a334be43fd5893f5b3
haseeAmarathunga <haseeamarathunga@gmail.com>
2018-11-22 12:12:40 -0800

    Update adminmenu

--- resources/views/adminmenu.blade.php
+++ resources/views/adminmenu.blade.php
@@ -14,19 +14,35 @@
     <script>window.location="/AdminLogin";</script>
 @endif
 @section('sidebar')
-<div class="well">
+<div class="jumbotron">
+    <a href="messages"><button class="btn btn-default btn-lg">
+    <span class="glyphicon glyphicon-envelope"></span> Forum Messages</button></a>
+    <a href="staffreg"><button class="btn btn-default btn-lg ">
+    <span class="glyphicon glyphicon-user"></span> Register Staff</button></a>
+    <a href=""><button class="btn btn-default btn-lg">
+    <span class="glyphicon glyphicon-eye-open"></span> View Staff Details</button></a>
+    <a href=""><button class="btn btn-default btn-lg">
+    <span class="glyphicon glyphicon-eye-open"></span> View Customers Details</button></a>
 </div>
 @endsection
 
 <!-- buttons -->
-
-<div>
-    <a href="messages"><button class="btn btn-success btn-lg btn-block">Forum Messages</button></a>
-    <a href="staffreg"><button class="btn btn-primary btn-lg btn-block">Register Staff</button></a>
-    <a href=""><button class="btn btn-success btn-lg btn-block">View Staff</button></a>
-    <a href=""><button class="btn btn-primary btn-lg btn-block">View Customers</button></a>
+<div class="navbar-header">
+<a href="messages">
+<img src="img/icon/message.png" width="200" height="120"></a>
+</div>
+<div class="navbar-header">
+<a href="staffreg">
+<img src="img/icon/regstaff.png" width="200" height="120"></a>
+</div>
+<div class="navbar-header">
+<a href="">
+<img src="img/icon/staff.png" width="200" height="120"></a>
+</div>
+<div class="navbar-header">
+<a href="">
+<img src="img/icon/customer.png" width="200" height="120"></a>
 </div>
 
-
-
+<hr>
 @endsection
