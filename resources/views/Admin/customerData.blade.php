caf6789643a6b64a0c0dcb6cec2d3b65b0673e0f
haseeAmarathunga <haseeamarathunga@gmail.com>
2018-11-23 17:42:39 -0800

    Update customersopp UI

--- resources/views/Admin/customerData.blade.php
+++ resources/views/Admin/customerData.blade.php
@@ -5,21 +5,21 @@
 
 <!-- check if admin loggedin -->
 @if(isset(Auth::user()->username))
-    <div class="alert alert-success success-block">
+    <!-- <div class="alert alert-success success-block">
         <strong>Welcome Admin</strong>
         <br/>
         <a href="{{url('AdminLogin/logout')}}">Logout</a>
-    </div>
+    </div> -->
 @else
     <script>window.location="/AdminLogin";</script>
 @endif
-
+<hr>
+<a href="/adminmenu"><button class="btn btn-default">back</button></a>
+<center><h2>Manage Customers</h2></center>
+<hr>
 <!-- buttons -->
 
 <div class="container">
-  <div class ="col-md-2 "></div>
-  <div class ="col-md-8">
-<center>
   <table class="table table-striped">
        <tr>
           <th>Customer NIC</th>
@@ -39,15 +39,12 @@
             <td><a href="{{url('cus_shedule',$row['nic'])}}" class="btn btn-primary btn-block btn-warning ">View & Create</a></td>
             <td><a href="{{url('cus_payment',$row['nic'])}}" class="btn btn-primary btn-block btn-info">pay</a></td>
             <td><a href="{{url('cus_delete')}}" class="btn btn-primary btn-block btn-danger ">Delete</a></td>
-              <td></td>    </tr>
-                 @endforeach
+            <td></td>    </tr>
+        @endforeach
 
   </table>
-</center>
 </div>
-    <div class ="col-md-2"></div>
-</div>
-
+<hr>
 
 
 @endsection
